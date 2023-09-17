2015a799356b858a13ed79723c0f339daeb5dcc6
BlackSoulHub <mr.asaglaev@mail.ru>
2023-09-17 12:19:00 +0300

    Move TariffInfo to Domain

---
+++ TestService.Domain/Models/TariffInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TestService.Services.Implementation;
+
+public class TariffInfo
+{
+    [JsonPropertyName("tariff_code")]
+    public required int Code { get; set; }
+    [JsonPropertyName("tariff_name")]
+    public required string Name { get; set; }
+    [JsonPropertyName("tariff_description")]
+    public required string Description { get; set; }
+    [JsonPropertyName("delivery_sum")]
+    public required float Cost { get; set; }
+    [JsonPropertyName("calendar_min")]
+    public required int MinDeliveryTime { get; set; }
+    [JsonPropertyName("calendar_max")]
+    public required int MaxDeliveryTime { get; set; }
+}
--- TestService.Services/Implementation/CalculatorService.cs
+++ TestService.Services/Implementation/CalculatorService.cs
@@ -138,22 +138,6 @@ file class GetTariffResponse
     public required IEnumerable<TariffInfo> Tariffs { get; set; }
 }
 
-public class TariffInfo
-{
-    [JsonPropertyName("tariff_code")]
-    public required int Code { get; set; }
-    [JsonPropertyName("tariff_name")]
-    public required string Name { get; set; }
-    [JsonPropertyName("tariff_description")]
-    public required string Description { get; set; }
-    [JsonPropertyName("delivery_sum")]
-    public required float Cost { get; set; }
-    [JsonPropertyName("calendar_min")]
-    public required int MinDeliveryTime { get; set; }
-    [JsonPropertyName("calendar_max")]
-    public required int MaxDeliveryTime { get; set; }
-}
-
 file class CalculationRequest
 {
     [JsonPropertyName("from_location")]
